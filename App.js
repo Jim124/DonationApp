6e6fc4aa3c07916eec73b16196d8cc8cf626b285
Jim124 <fengpingan065@gmail.com>
2024-06-11 14:27:11 +0800

    initialize Donation project

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'inter-Black': require('./assets/fonts/Inter-Black.ttf'),
+    'inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+    'inter-ExtraBold': require('./assets/fonts/Inter-ExtraBold.ttf'),
+    'inter-ExtraLight': require('./assets/fonts/Inter-ExtraLight.ttf'),
+    'inter-Medium': require('./assets/fonts/Inter-Medium.ttf'),
+    'inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+    'inter-SemiBold': require('./assets/fonts/Inter-SemiBold.ttf'),
+    'inter-Thin': require('./assets/fonts/Inter-Thin.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+      <StatusBar style='auto' />
+    </>
   );
 }
 
---
+++ assets/styles/globalStyle.js
@@ -0,0 +1,6 @@
+import { StyleSheet } from 'react-native';
+import { getFontFamily } from '../fonts/helper';
+
+import { horizontalScale, scaleFontSize, verticalScale } from './scaling';
+const globalStyle = StyleSheet.create({});
+export default globalStyle;
