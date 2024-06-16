297ec979dca2a69af1287b2d8660a407092d36f3
Jim124 <fengpingan065@gmail.com>
2024-06-16 15:18:23 +0800

    add stripe cardForm

---
+++ constants/App.js
@@ -0,0 +1,6 @@
+const Constants = {
+  STRIPE_PUBLIC_KEY:
+    'pk_test_51NW7xFFRAKA8j90mbAE00MKAqnZb4MLHNRSad6xy2qIZuAQLLzcuFQRYnGoIG5lAXvBKWeEPiVnafCd1OOcWaf0p00y3tLPpap',
+};
+
+export default Constants;
--- screens/Payment/Payment.js
+++ screens/Payment/Payment.js
@@ -1,10 +1,12 @@
 import { useSelector } from 'react-redux';
 import { SafeAreaView, ScrollView, View, Text } from 'react-native';
+import { StripeProvider, CardForm } from '@stripe/stripe-react-native';
 
 import style from './style';
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
+import Constants from '../../constants/App';
 const Payment = () => {
   const donationItem = useSelector(
     (state) => state.donations.selectedDonationInformation
@@ -17,6 +19,11 @@ const Payment = () => {
           <Text style={style.price}>
             You are abount to donate {donationItem.price}
           </Text>
+        </View>
+        <View>
+          <StripeProvider publishableKey={Constants.STRIPE_PUBLIC_KEY}>
+            <CardForm style={style.cardForm} />
+          </StripeProvider>
         </View>
       </ScrollView>
       <View style={style.button}>
