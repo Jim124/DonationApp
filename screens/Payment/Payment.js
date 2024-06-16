dd849482d20ba5c9ab55e5f220a57f059b079876
Jim124 <fengpingan065@gmail.com>
2024-06-16 18:12:09 +0800

    add stripe payment

--- screens/Payment/Payment.js
+++ screens/Payment/Payment.js
@@ -1,16 +1,44 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { SafeAreaView, ScrollView, View, Text } from 'react-native';
-import { StripeProvider, CardForm } from '@stripe/stripe-react-native';
+import { SafeAreaView, ScrollView, View, Text, Alert } from 'react-native';
+import {
+  StripeProvider,
+  CardForm,
+  useConfirmPayment,
+} from '@stripe/stripe-react-native';
 
 import style from './style';
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
 import Constants from '../../constants/App';
-const Payment = () => {
+import { fetchPaymentIntentClientSecret } from '../../api/stripePayment';
+const Payment = ({ navigation }) => {
+  const [isReady, setIsReady] = useState(false);
   const donationItem = useSelector(
     (state) => state.donations.selectedDonationInformation
   );
+  const user = useSelector((state) => state.user);
+  const { confirmPayment, loading } = useConfirmPayment();
+  async function handlePayment() {
+    const clientSecret = await fetchPaymentIntentClientSecret(
+      user.email,
+      donationItem.price
+    );
+    console.log(clientSecret);
+    const { error, paymentIntent } = await confirmPayment(clientSecret, {
+      paymentMethodType: 'Card',
+    });
+    if (error) {
+      Alert.alert(
+        'Error has occured with your payment',
+        error.localizedMessage
+      );
+    } else if (paymentIntent) {
+      Alert.alert('Successful', 'The payment was confirmed successfully!');
+      navigation.goBack();
+    }
+  }
   return (
     <SafeAreaView style={[globalStyle.backgroundColor, globalStyle.flex]}>
       <ScrollView style={style.paymentContainer}>
@@ -22,12 +50,21 @@ const Payment = () => {
         </View>
         <View>
           <StripeProvider publishableKey={Constants.STRIPE_PUBLIC_KEY}>
-            <CardForm style={style.cardForm} />
+            <CardForm
+              style={style.cardForm}
+              onFormComplete={() => {
+                setIsReady(true);
+              }}
+            />
           </StripeProvider>
         </View>
       </ScrollView>
       <View style={style.button}>
-        <Button title='Donate' />
+        <Button
+          title='Donate'
+          isDisabled={!isReady || loading}
+          onPress={handlePayment}
+        />
       </View>
     </SafeAreaView>
   );
