ede4c8d50bc98ddd4e1088f2f3615fd777813a77
Jim124 <fengpingan065@gmail.com>
2024-06-17 10:05:25 +0800

    add .env environment file

--- api/stripePayment.js
+++ api/stripePayment.js
@@ -1,19 +1,17 @@
 export const fetchPaymentIntentClientSecret = async (email, amount) => {
-  const response = await fetch(
-    'https://catpayment-recm6sndva-uc.a.run.app/create-payment-intent',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({
-        email: email,
-        currency: 'usd',
-        amount: parseInt(amount) * 100,
-      }),
-    }
-  );
+  const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      email: email,
+      currency: 'usd',
+      amount: parseInt(amount) * 100,
+    }),
+  });
   const { clientSecret } = await response.json();
   return clientSecret;
 };
